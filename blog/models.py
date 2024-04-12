d15a34f2a977c1f62d30a6151c9e4c3da3d3ad54
akashdeeep <akashdeep@iitbhilai.ac.in>
2024-04-12 10:04:36 +0530

    added author and post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.TextField(max_length=200)
+    image_name = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
